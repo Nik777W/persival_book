271c4512b2ce81422e9d75577fbe53ffa920938b
evgenii <paderov.e@gmail.com>
2025-03-07 18:36:35 -0300

    add unittest

--- persival_book/functional_test.py
+++ persival_book/functional_test.py
@@ -1,10 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import unittest
 
-options = Options()
-options.add_argument("--headless")
+# options = Options()
+# options.add_argument("--headless")
 
-brauser = webdriver.Chrome(options=options)
-brauser.get('http://localhost:8000')
-assert 'The install worked successfully!' in brauser.title
-print(brauser.title)
+# brauser = webdriver.Chrome(options=options)
+# brauser.get('http://localhost:8000')
+# assert 'To-Do' in brauser.title, '' + brauser.title
+# print(brauser.title)
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument("--headless")
+        self.brauser = webdriver.Chrome(options=options)
+
+    def tearDown(self):
+        self.brauser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.brauser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.brauser.title)
+        self.fail('"Тест остановлен на данном этапе"')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
