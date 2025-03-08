250067c44f1bd9671234c8c05b652951ee19006c
evgenii <paderov.e@gmail.com>
2025-03-07 22:28:08 -0300

    функциональный тест проверяет возможность вести элемент списка

--- persival_book/functional_test.py
+++ persival_book/functional_test.py
@@ -1,30 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import unittest
-
-# options = Options()
-# options.add_argument("--headless")
-
-# brauser = webdriver.Chrome(options=options)
-# brauser.get('http://localhost:8000')
-# assert 'To-Do' in brauser.title, '' + brauser.title
-# print(brauser.title)
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
+        """ установка """
         options = Options()
         options.add_argument("--headless")
-        self.brauser = webdriver.Chrome(options=options)
+        self.browser = webdriver.Chrome(options=options)
 
     def tearDown(self):
-        self.brauser.quit()
+        """ демонтаж """
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.brauser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.brauser.title)
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Marina', self.browser.title)
+        header_text = self.browser.find_element('tag_name','h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('pleaceholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element('id', 'id_list_table')
+        rows = table.find_element('tag_name', 'tr')
+        self.assertTrue(row.text == '1: купить павлиньи перья' for row in rows)
+
+
         self.fail('"Тест остановлен на данном этапе"')
+
+
 
 
 if __name__ == '__main__':
